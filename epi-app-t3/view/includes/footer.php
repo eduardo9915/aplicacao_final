e3d33f5e8cd4c6d0ac5c82626b583d11c1b98367
eduardo9915 <natividadepauloeduardo@gmail.com>
2026-01-20 22:11:49 -0300

    Delete epi-app-t3 directory

--- epi-app-t3/view/includes/footer.php
+++
@@ -1,7 +0,0 @@
-    <footer class="footer">
-        <div class="footer-content">
-            <p>&copy; <?php echo date('Y'); ?> Sistema de Gestão EPI/EPC. Todos os direitos reservados.</p>
-        </div>
-    </footer>
-</body>
-</html>
